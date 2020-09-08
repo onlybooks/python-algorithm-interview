c5af14f6a81406b8f2be2cffb11fe3a223da6acf
Sang-Kil Park <likejazz@gmail.com>
2020-09-08 14:46:05 +0900

    Update 88-2.py

--- 5-algorithms/ch23/88-2.py
+++ 5-algorithms/ch23/88-2.py
@@ -10,7 +10,7 @@ class Solution:
             return max(nums)
 
         dp = collections.OrderedDict()
-        dp[0], dp[1] = nums[0], max(dp[0], dp[1])
+        dp[0], dp[1] = nums[0], max(nums[0], nums[1])
         for i in range(2, len(nums)):
             dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
 
