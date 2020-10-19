aa2c4e644925f777bd7c846bdca8390ae97bb1a5
Sang-Kil Park <likejazz@gmail.com>
2020-10-19 18:43:35 +0900

    #38 Fix inefficient approach.

--- 2-python/ch06/6-1.py
+++ 2-python/ch06/6-1.py
@@ -2,10 +2,10 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         # 팰린드롬 판별 및 투 포인터 확장
         def expand(left: int, right: int) -> str:
-            while left >= 0 and right <= len(s) and s[left] == s[right - 1]:
+            while left >= 0 and right < len(s) and s[left] == s[right]:
                 left -= 1
                 right += 1
-            return s[left + 1:right - 1]
+            return s[left + 1:right]
 
         # 해당 사항이 없을때 빠르게 리턴
         if len(s) < 2 or s == s[::-1]:
