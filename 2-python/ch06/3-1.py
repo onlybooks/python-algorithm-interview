c48b7ada852abbb8514ccc4304aa43ebdc1b6ac3
sdk <admin@sundo.kim>
2021-05-15 19:47:23 +0900

    📝 질문 :

--- 2-python/ch06/3-1.py
+++ 2-python/ch06/3-1.py
@@ -11,5 +11,5 @@ class Solution:
                 letters.append(log)
 
         # 두 개의 키를 람다 표현식으로 정렬
-        letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
+        letters.sort(key=lambda x: (x.split()[1], x.split()[0]))
         return letters + digits
